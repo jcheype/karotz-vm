4d8a2a359e26d19f53a43fdad1d132248895d708
Julien Cheype <cheype@gmail.com>
2011-07-09 01:15:13 +0200

    add ears functions

--- src/main/resources/karotz.js
+++ src/main/resources/karotz.js
@@ -57,6 +57,20 @@ karotz.led.pulse = function(color, period, duration, callback){
 }
 
 //////
+karotz.ears = {}
+karotz.ears.move = function(left, right, callback){
+    log("karotz ears move: [" + left + "," + right + "]");
+    __CLIENT__.sendEarsMove(left, right, __GEN_VOOS_CALLBACK(callback));
+}
+karotz.ears.moveRelative = function(left, right, callback){
+    log("karotz ears moveRelative: [" + left + "," + right + "]");
+    __CLIENT__.sendEarsMoveRelative(left, right, __GEN_VOOS_CALLBACK(callback));
+}
+karotz.ears.reset = function(callback){
+    log("karotz ears reset");
+    __CLIENT__.sendEarsReset(__GEN_VOOS_CALLBACK(callback));
+}
+//////
 karotz.webcam = {}
 karotz.webcam.photo = function(url){
     log("karotz webcam photo: " + url);
@@ -69,7 +83,76 @@ karotz.multimedia.play = function( path, callback){
     log("karotz multimedia play: " + path);
     __CLIENT__.sendMultimediaPlay(path, __GEN_VOOS_CALLBACK(callback));
 }
+karotz.multimedia.pause = function(callback){
+    log("karotz multimedia pause");
+    __CLIENT__.sendMultimediaPause(__GEN_VOOS_CALLBACK(callback));
+}
+karotz.multimedia.resume = function(callback){
+    log("karotz multimedia resume");
+    __CLIENT__.sendMultimediaResume(__GEN_VOOS_CALLBACK(callback));
+}
+karotz.multimedia.stop = function(callback){
+    log("karotz multimedia stop");
+    __CLIENT__.sendMultimediaStop(__GEN_VOOS_CALLBACK(callback));
+}
+karotz.multimedia.next = function(callback){
+    log("karotz multimedia next");
+    __CLIENT__.sendMultimediaNext(__GEN_VOOS_CALLBACK(callback));
+}
+karotz.multimedia.previous = function(callback){
+    log("karotz multimedia previous");
+    __CLIENT__.sendMultimediaPrevious(__GEN_VOOS_CALLBACK(callback));
+}
 
+///////////
+karotz.multimedia.artist = function(callback){
+    log("karotz multimedia artist");
+    var voosCallback = null;
+    if(callback){
+        voosCallback = new net.violet.karotz.client.VoosCallBack() {
+            onEvent: function(voosMsg){
+                callback(voosMsg.getMultimedia().getArtistlistList().get(0))
+            }
+        };
+    }
+    __CLIENT__.sendMultimediaArtist(voosCallback);
+}
+karotz.multimedia.folder = function(callback){
+    log("karotz multimedia folder");
+    var voosCallback = null;
+    if(callback){
+        voosCallback = new net.violet.karotz.client.VoosCallBack() {
+            onEvent: function(voosMsg){
+                callback(voosMsg.getMultimedia().getFolderlistList().get(0))
+            }
+        };
+    }
+    __CLIENT__.sendMultimediaFolder(voosCallback);
+}
+karotz.multimedia.genre = function(callback){
+    log("karotz multimedia genre");
+    var voosCallback = null;
+    if(callback){
+        voosCallback = new net.violet.karotz.client.VoosCallBack() {
+            onEvent: function(voosMsg){
+                callback(voosMsg.getMultimedia().getGenrelistList().get(0))
+            }
+        };
+    }
+    __CLIENT__.sendMultimediaGenre(voosCallback);
+}
+karotz.multimedia.song = function(callback){
+    log("karotz multimedia song");
+    var voosCallback = null;
+    if(callback){
+        voosCallback = new net.violet.karotz.client.VoosCallBack() {
+            onEvent: function(voosMsg){
+                callback(voosMsg.getMultimedia().getSonglistList().get(0))
+            }
+        };
+    }
+    __CLIENT__.sendMultimediaSong(voosCallback);
+}
 //////
 karotz.asr = {}
 karotz.asr.string= function(grammar, lang, callback){
@@ -82,8 +165,7 @@ karotz.asr.string= function(grammar, lang, callback){
             onEvent: function(voosMsg){
                 if(!voosMsg.hasAsrCallback())
                     return;
-                log("RECO: " + voosMsg.getAsrCallback().getRecognitionCount());
-                log("RECO: " + voosMsg.toString());
+                log("ASR reco: " + voosMsg.getAsrCallback().getRecognitionCount());
                 var tmpResult = voosMsg.getAsrCallback().getRecognition(0);
 
                 var asrResult = {
--- src/test/resources/testKarotz.js
+++ src/test/resources/testKarotz.js
@@ -14,21 +14,25 @@ cbPulse = function(event){
     }
 }
 
-karotz.connect("192.168.0.22", 9123);
+karotz.connect("10.104.2.41", 9123);
 karotz.start(callback, "test");
 
 
 karotz.button.addListener(function(event){ log(event) });
 
 //
+karotz.ears.moveRelative(null, 10, function(event){ log(event) });
 
 karotz.tts.start("hello tout le monde", "fr", function(event){
     if(event=="TERMINATED"){
-        karotz.multimedia.play("http://www.universal-soundbank.com/mp3/sounds/829.mp3", function(event){
+        karotz.multimedia.artist(function(event){
+         log(event)
+         karotz.multimedia.play("http://www.universal-soundbank.com/mp3/sounds/829.mp3", function(event){
              log(event);
              if(event == "TERMINATED")
                  karotz.led.pulse("0000FF", 100, 3000, cbPulse);
          })
+        })
     }
 });
 karotz.led.light("FF0000");
